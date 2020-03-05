235a3067682c416e1623dbbd52c43173191b45ea
Kyle Wong <kylewonga@gmail.com>
2020-03-04 20:41:15 -0800

    Fixed prjt addUser path+differentiate length errors by input

--- errors.js
+++ errors.js
@@ -1,4 +1,4 @@
-const logger = require('./logger');
+const logger = require("./logger");
 const maxUsernameLength = 32;
 const minPasswordLength = 6; //inclusive
 const maxPasswordLength = 32; //inclusive
@@ -6,62 +6,56 @@ const maxEmailLength = 32;
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const tokenLength = 32;
 
-
-
 exports.DONE = 0;
 exports.ERROR = 400;
 exports.SESSION_NOT_FOUND = 408;
 exports.SESSION_IS_CLOSED = 409;
 
+exports.errorCode = {
+  0: "Done",
 
-exports.errorCode = 
-{
-    0:"Done",
+  400: "Error",
+  401: "Invalid Length",
+  402: "Regex Fail",
+  403: "Invalid Type",
+  404: "Username already exists",
+  405: "Username not found",
+  406: "Insufficient Privilege",
+  407: "Incorrect Login Credentials",
+  408: "Session not found",
+  409: "Session is closed",
+  418: "Username-invalid length",
+  419: "Password-invalid length",
+  420: "Email-invalid length"
+};
+exports.validUsername = function(username) {
+  if (typeof username != "string") return 403;
+  if (username.length <= 0 || username.length > maxUsernameLength) return 418;
 
+  return 0;
+};
+exports.validPassword = function(password) {
+  if (typeof password != "string") return 403;
+  if (
+    password.length < minPasswordLength ||
+    password.length > maxPasswordLength
+  )
+    return 419;
 
-    400:"Error",
-    401:"Invalid Length",
-    402:"Regex Fail",
-    403:"Invalid Type",
-    404:"Username already exists",
-    405:"Username not found",
-    406:"Insufficient Privilege",
-    407:"Incorrect Login Credentials",
-    408:"Session not found",
-    409:"Session is closed",
-}
-exports.validUsername = function(username){
-    if(typeof username != 'string')
-        return 403;
-    if(username.length <= 0 || username.length > maxUsernameLength)
-        return 401;
-    
-    return 0;
-}
-exports.validPassword = function(password){
-    if(typeof password != 'string')
-        return 403;
-    if(password.length < minPasswordLength || password.length > maxPasswordLength)
-        return 401;
-    
-    return 0;
-}
+  return 0;
+};
 
-exports.validEmail = function(email){
-    if(typeof email != 'string')
-        return 403;
-    if(email.length <= 0 || email.length > maxEmailLength)
-        return 401;
+exports.validEmail = function(email) {
+  if (typeof email != "string") return 403;
+  if (email.length <= 0 || email.length > maxEmailLength) return 420;
 
-    if(!(emailRegex.test(email))){
-        return 402;
-    }
-    return 0;
-}
-exports.validToken = function(token){
-    if(typeof token != 'string')
-        return 403;
-    if(token.length != tokenLength)
-        return 401;
-    return 0;
-}
+  if (!emailRegex.test(email)) {
+    return 402;
+  }
+  return 0;
+};
+exports.validToken = function(token) {
+  if (typeof token != "string") return 403;
+  if (token.length != tokenLength) return 401;
+  return 0;
+};
--- user.js
+++ user.js
@@ -1,11 +1,11 @@
-const logger = require('./logger');
-const security = require('./password');
-const errors = require('./errors');
-const session = require('./session');
+const logger = require("./logger");
+const security = require("./password");
+const errors = require("./errors");
+const session = require("./session");
 const pool = require("./database");
 
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const https = require("https");
 const DEFAULT_ACCOUNT_TYPE = 5;
 
 const USERNAME_ALREADY_EXISTS = 404;
@@ -13,194 +13,187 @@ const USERNAME_NOT_FOUND = 405;
 const INSUFFICIENT_PRIVILEGE = 406;
 const INCORRECT_LOGIN_CREDENTIALS = 407;
 
-const config = require('config');
-const projectServerConfig = config.get('projectServerConfig'); 
+const config = require("config");
+const projectServerConfig = config.get("projectServerConfig");
 
+exports.register = async function(resBuilder, email, username, password) {
+  //basic checks
+  var errorCode = errors.validUsername(username);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  errorCode = errors.validEmail(email);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  errorCode = errors.validPassword(password);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
 
-exports.register = async function(resBuilder, email, username, password){
-    //basic checks
-    var errorCode = errors.validUsername(username);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
+  //hash password
+  let { salt, hash } = security.hashPassword(password);
+
+  //check if username exists
+  try {
+    const usernameAvailable = await isUsernameAvailable(username);
+    if (usernameAvailable) {
+      //insert user record is username is available
+      logger.log("available");
+      await insertUser(username, email, hash, salt);
+      //add user to the projects server
+      addUserToProjectsServer(username, email, 1, DEFAULT_ACCOUNT_TYPE);
+    } else {
+      logger.log("not available");
+      return resBuilder.default(USERNAME_ALREADY_EXISTS).end();
     }
-    errorCode = errors.validEmail(email);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
+  } catch (err) {
+    logger.log(err);
+    return resBuilder.error(err).end();
+  }
+
+  try {
+  } catch (err) {
+    logger.log(err);
+  }
+
+  return resBuilder.success().end();
+};
+
+exports.login = async function(resBuilder, username, password) {
+  //basic checks
+  var errorCode = errors.validUsername(username);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  errorCode = errors.validPassword(password);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+
+  //check is password is correct
+  try {
+    const loginSuccess = await passwordMatch(username, password);
+    if (loginSuccess) {
+      logger.log("Credential match for " + username);
+      //get current active session if it exists
+      const activeSession = await session.retrieveActiveSession(username);
+      if (activeSession != null) {
+        //revoke active session if it exists
+        session.revokeSession(username, activeSession.session_id);
+      }
+      //Generate a new session
+      const newSession = session.generateSession(username);
+      await session.insertSession(newSession);
+      resBuilder.json["Session"] = newSession;
+      return resBuilder.success().end();
+    } else {
+      return resBuilder.default(INCORRECT_LOGIN_CREDENTIALS).end();
     }
-    errorCode = errors.validPassword(password);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
-        
+  } catch (err) {
+    logger.log("ERR:" + err.stack);
+    return resBuilder.error(err).end();
+  }
+};
+exports.verifyPrivilege = async function(
+  resBuilder,
+  username,
+  required_privilege
+) {
+  var errorCode = errors.validUsername(username);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  const code = await exports.comparePrivilege(username, required_privilege);
+  resBuilder.default(code).end();
+};
+exports.comparePrivilege = async function(username, required_privilege) {
+  var query = `SELECT account_type FROM users WHERE username = ?`;
+  try {
+    const rows = await pool.query(query, [username]);
+    if (rows.length == 0) {
+      //user not found
+      return USERNAME_NOT_FOUND;
     }
-
-    //hash password
-    let {salt, hash} = security.hashPassword(password);
-
-    //check if username exists
-    try{
-        const usernameAvailable = await isUsernameAvailable(username);
-        if(usernameAvailable){
-            //insert user record is username is available
-            logger.log("available");
-            await insertUser(username,email,hash,salt);
-            //add user to the projects server
-            addUserToProjectsServer(username,email,1,DEFAULT_ACCOUNT_TYPE);
-        } else {
-            logger.log("not available");
-            return resBuilder.default(USERNAME_ALREADY_EXISTS).end();
-        }
-    } catch(err){
-        logger.log(err);
-        return resBuilder.error(err).end();
+    if (rows[0].account_type <= required_privilege) {
+      //user has sufficient privilege
+      return 0;
+    } else {
+      //user does not have sufficient privilege
+      return INSUFFICIENT_PRIVILEGE;
     }
+  } catch (err) {
+    logger.log(err.stack);
+    return 400;
+  }
+};
 
-    try{
-
-    } catch(err){
-        logger.log(err);
+async function insertUser(username, email, hash, salt) {
+  logger.log(`Registering ${username} with email=${email}`);
+  var query = `INSERT INTO users VALUES(?,?,?,?,1,${DEFAULT_ACCOUNT_TYPE})`;
+  try {
+    await pool.query(query, [username, email, hash, salt]);
+    return;
+  } catch (err) {
+    throw err;
+  }
+}
+async function isUsernameAvailable(username) {
+  var query = "SELECT username FROM users u WHERE u.username = ?;";
+  try {
+    const rows = await pool.query(query, [username]);
+    console.log(rows);
+    if (rows.length > 0) {
+      logger.log(`${username} already exists.`);
+      return false;
+    } else {
+      logger.log(`${username} is available.`);
+      return true;
     }
-
-
-    return resBuilder.success().end();
-
+  } catch (err) {
+    throw err;
+  }
+}
+async function passwordMatch(username, password) {
+  var query =
+    "SELECT salt, hash FROM users WHERE username = ? AND is_active = 1";
+  try {
+    const rows = await pool.query(query, [username]);
+    if (rows.length == 0) {
+      return false;
+    }
+    logger.log(rows[0]);
+    return security.validatePassword(password, rows[0].salt, rows[0].hash);
+  } catch (err) {
+    throw err;
+  }
 }
 
-exports.login = async function(resBuilder, username, password){
-    //basic checks
-    var errorCode = errors.validUsername(username);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
+function addUserToProjectsServer(username, email, isActive, accountType) {
+  logger.log("Sending addUser request to Projects server");
+  const jsonBody = {
+    username: username,
+    email: email,
+    is_active: isActive,
+    account_type: accountType
+  };
+  const options = {
+    host: projectServerConfig.host,
+    port: projectServerConfig.port,
+    path: "/prjt/user/add",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
     }
-    errorCode = errors.validPassword(password);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
-    }
+  };
+  logger.log(options);
+  //use https library if using port 443
+  const port = options.port == 443 ? https : http;
 
-    //check is password is correct
-    try{
-        const loginSuccess = await passwordMatch(username, password);
-        if(loginSuccess){
-            logger.log("Credential match for " + username);
-            //get current active session if it exists
-            const activeSession = await session.retrieveActiveSession(username);
-            if(activeSession != null){
-                //revoke active session if it exists
-                session.revokeSession(username,activeSession.session_id);
-            }
-            //Generate a new session
-            const newSession = session.generateSession(username);
-            await session.insertSession(newSession);
-            resBuilder.json["Session"] = newSession;
-            return resBuilder.success().end();
-
-        } else {
-            return resBuilder.default(INCORRECT_LOGIN_CREDENTIALS).end();
-        }
-    } catch(err){
-        logger.log("ERR:" + err.stack);
-        return resBuilder.error(err).end();
-    }
-
+  const req = http.request(options, res => {});
+  req.on("error", err => {
+    logger.log(`Error adding ${user} to project server`);
+  });
+  req.write(JSON.stringify(jsonBody));
+  req.end();
 }
-exports.verifyPrivilege = async function(resBuilder, username, required_privilege){
-    var errorCode = errors.validUsername(username);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
-    }
-    const code = await(exports.comparePrivilege(username, required_privilege));
-    resBuilder.default(code).end();
-}
-exports.comparePrivilege = async function(username, required_privilege){
-
-    var query = `SELECT account_type FROM users WHERE username = ?`;
-    try{
-        const rows = await pool.query(query,[username]);
-        if(rows.length == 0){
-            //user not found
-            return USERNAME_NOT_FOUND;
-        }
-        if(rows[0].account_type <= required_privilege){
-            //user has sufficient privilege
-            return 0;
-        } else {
-            //user does not have sufficient privilege
-            return INSUFFICIENT_PRIVILEGE;
-        }
-    } catch(err){
-        logger.log(err.stack);
-        return 400;
-    }
-}
-
-async function insertUser(username, email, hash, salt){
-    logger.log(`Registering ${username} with email=${email}`)
-    var query = `INSERT INTO users VALUES(?,?,?,?,1,${DEFAULT_ACCOUNT_TYPE})`
-    try{
-        await pool.query(query,[username,email,hash,salt]);
-        return;
-    } catch(err){
-        throw err;
-    }
-    
-}
-async function isUsernameAvailable(username){
-    var query = "SELECT username FROM users u WHERE u.username = ?;";
-    try{
-        const rows = await pool.query(query,[username]);
-        console.log(rows);
-        if(rows.length > 0){
-            logger.log(`${username} already exists.`);
-            return false;
-        } else {
-            logger.log(`${username} is available.`);
-            return true;
-        }
-    } catch(err){
-        throw err;
-    }
-}
-async function passwordMatch(username, password){
-    var query = "SELECT salt, hash FROM users WHERE username = ? AND is_active = 1";
-    try{
-        const rows = await pool.query(query,[username]);
-        if(rows.length == 0){
-            return false;
-        }
-        logger.log(rows[0]);
-        return security.validatePassword(password, rows[0].salt, rows[0].hash);
-    } catch(err){
-        throw err;
-    }
-}
-
-function addUserToProjectsServer(username, email, isActive, accountType){
-    logger.log("Sending addUser request to Projects server");
-    const jsonBody = {
-        username:username,
-        email:email,
-        is_active:isActive,
-        account_type:accountType
-    };
-    const options = {
-        host: projectServerConfig.host,
-        port: projectServerConfig.port,
-        path: "/addUser",
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }
-    logger.log(options);
-    //use https library if using port 443
-    const port = (options.port == 443) ? https : http;
-    
-    const req = http.request(options, (res)=>{});
-    req.on('error', (err) => {
-        logger.log(`Error adding ${user} to project server`);
-    })
-    req.write(JSON.stringify(jsonBody));
-    req.end();
-}
-
-
-
