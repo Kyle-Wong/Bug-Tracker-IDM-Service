3b5095ddef8919351ddd63f181d3a250e7254871
Kyle Wong <kylewonga@gmail.com>
2020-03-14 16:00:13 -0700

    add logout endpoint

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const util = require('util');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const util = require("util");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-const fs = require('fs');
-const logger = require('./logger');
-const ResponseBuilder = require('./response-builder');
-const user = require('./user');
-const session = require('./session');
+const fs = require("fs");
+const logger = require("./logger");
+const ResponseBuilder = require("./response-builder");
+const user = require("./user");
+const session = require("./session");
 
-const config = require('config');
-const dbConfig = config.get('dbConfig');
+const config = require("config");
+const dbConfig = config.get("dbConfig");
 logger.log(dbConfig);
-const serverConfig = config.get('serverConfig');
+const serverConfig = config.get("serverConfig");
 logger.log(serverConfig);
 var mysql = require("mysql");
 
-
-app.use(express.static('public'))
-app.use(bodyParser.json())
+app.use(express.static("public"));
+app.use(bodyParser.json());
 
 /*
 const con = mysql.createConnection(dbConfig);
@@ -30,44 +29,55 @@ con.connect(function(err){
   logger.log("IDM database connected!");
 });
 */
-const pool = require('./database');
+const pool = require("./database");
 
-app.get('/', function(req, res){
-    responseBuilder = new ResponseBuilder(res);
-    
-})
+app.get("/", function(req, res) {
+  responseBuilder = new ResponseBuilder(res);
+});
 
-var server = app.listen(serverConfig.port, function(){
+var server = app.listen(serverConfig.port, function() {
   var host = server.address().address;
   var port = server.address().port;
   logger.log(`Server listening at ${host}:${port}`);
-})
+});
 
-
-app.post('/idm/user/register', function(req,res){
-  logger.log("Register User")
+app.post("/idm/user/register", function(req, res) {
+  logger.log("Register User");
   body = req.body;
   logger.log(req.body);
-  user.register(new ResponseBuilder(res),body.email,body.username,body.password);
+  user.register(
+    new ResponseBuilder(res),
+    body.email,
+    body.username,
+    body.password
+  );
 });
-app.post('/idm/user/login', (req, res) =>{
+app.post("/idm/user/login", (req, res) => {
   logger.log("Login User");
   body = req.body;
   logger.log(req.body);
   user.login(new ResponseBuilder(res), body.username, body.password);
 });
-
-app.post('/idm/verifySession', function(req, res){
+app.post("/idm/user/logout", (req, res) => {
+  logger.log("Logout User");
+  body = req.body;
+  logger.log(req.body);
+  session.logout(new ResponseBuilder(res), body.username);
+});
+app.post("/idm/verifySession", function(req, res) {
   logger.log("Verify Session");
   body = req.body;
   logger.log(body);
-  session.verifySession(new ResponseBuilder(res), body.username, body.sessionID);
+  session.verifySession(new ResponseBuilder(res), body.username, body.session);
 });
 
-app.post('/idm/verifyPrivilege',function(req,res){
+app.post("/idm/verifyPrivilege", function(req, res) {
   logger.log("Verify Privilege");
   body = req.body;
   logger.log(body);
-  user.verifyPrivilege(new ResponseBuilder(res), body.username, body.requiredPrivilege);
+  user.verifyPrivilege(
+    new ResponseBuilder(res),
+    body.username,
+    body.requiredPrivilege
+  );
 });
-
--- session.js
+++ session.js
@@ -1,97 +1,124 @@
-const config = require('config');
-const logger = require('./logger');
-const errors = require('./errors');
+const config = require("config");
+const logger = require("./logger");
+const errors = require("./errors");
 const pool = require("./database");
-const sessionConfig = config.get('sessionConfig');
-const moment = require('moment');
-var crypto = require('crypto');
+const sessionConfig = config.get("sessionConfig");
+const moment = require("moment");
+var crypto = require("crypto");
 const sessionBytes = 16;
 
-exports.verifySession = async function(resBuilder, username, sessionID){
-    /*
+exports.verifySession = async function(resBuilder, username, sessionID) {
+  /*
         Verify if session exists and is active.
 
         If session exists and is active, update the last_used_time and expiry_time.
         If session exists but has timed out, mark session as inactive
         If session doesn't exist, return failure.
     */
-    var errorCode = errors.validUsername(username);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
-    }
-    errorCode = errors.validToken(sessionID);
-    if(errorCode != 0){
-        return resBuilder.default(errorCode).end();
-    }
-    //get active session
-    const sess = await exports.retrieveActiveSession(username);
+  var errorCode = errors.validUsername(username);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  errorCode = errors.validToken(sessionID);
+  if (errorCode != 0) {
+    return resBuilder.default(errorCode).end();
+  }
+  //get active session
+  const sess = await exports.retrieveActiveSession(username);
 
-    //return fail if session isn't found
-    if(sess == null || sess.session_id != sessionID)
-        return resBuilder.default(errors.SESSION_NOT_FOUND).end();
-    
-    if(moment(sess.lastUsedTime).isAfter(sess.expiryTime)){
-        //timeout
-        exports.revokeSession(username,sessionID)
-        return resBuilder.default(errors.SESSION_IS_CLOSED).end();
-    } else {
-        //valid session, update lastUsedTime
-        sess.lastUsedTime = moment().format('YYYY-MM-DD HH:mm:ss');
-        sess.expiryTime = moment(Date.now()).add(sessionConfig.timeout,'ms').format('YYYY-MM-DD HH:mm:ss');
-        exports.updateSession(sess);
-        return resBuilder.default(errors.DONE).end();
-    }
+  //return fail if session isn't found
+  if (sess == null || sess.sessionID != sessionID)
+    return resBuilder.default(errors.SESSION_NOT_FOUND).end();
 
+  if (moment(Date.now()).isAfter(sess.expiryTime)) {
+    //timeout
+    exports.revokeSession(username, sessionID);
+    return resBuilder.default(errors.SESSION_IS_CLOSED).end();
+  } else {
+    //valid session, update lastUsedTime
+    sess.lastUsedTime = moment().format("YYYY-MM-DD HH:mm:ss");
+    sess.expiryTime = moment(Date.now())
+      .add(sessionConfig.timeout, "ms")
+      .format("YYYY-MM-DD HH:mm:ss");
+    exports.updateSession(sess);
+    return resBuilder.default(errors.DONE).end();
+  }
+};
+exports.generateSession = function(username) {
+  var session = {};
+  session.username = username;
+  session.sessionID = crypto.randomBytes(sessionBytes).toString("hex");
+  session.createTime = moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
+  session.lastUsedTime = session.createTime;
+  session.expiryTime = moment(Date.now())
+    .add(sessionConfig.timeout, "ms")
+    .format("YYYY-MM-DD HH:mm:ss");
+  session.isActive = 1;
 
-
-}
-exports.generateSession = function(username){
-    var session = {}
-    session.username = username;
-    session.sessionID = crypto.randomBytes(sessionBytes).toString('hex');
-    session.createTime = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
-    session.lastUsedTime = session.createTime;
-    session.expiryTime = moment(Date.now()).add(sessionConfig.timeout,'ms').format('YYYY-MM-DD HH:mm:ss');
-    session.isActive = 1;
-
-    return session;
-}
-exports.updateSession = async function(sess){
-    var query = `UPDATE sessions SET last_used_time = ?, expiry_time = ?, is_active = ?
+  return session;
+};
+exports.updateSession = async function(sess) {
+  var query = `UPDATE sessions SET last_used_time = ?, expiry_time = ?, is_active = ?
                     WHERE username = ? AND session_id = ?`;
-    try{
-        await pool.query(query,[sess.lastUsedTime,sess.expiryTime,
-            sess.isActive,sess.username,sess.sessionID]);
-    } catch(err){
-        throw err;
-    }
-    
-}
+  try {
+    await pool.query(query, [
+      sess.lastUsedTime,
+      sess.expiryTime,
+      sess.isActive,
+      sess.username,
+      sess.sessionID
+    ]);
+  } catch (err) {
+    throw err;
+  }
+};
 
-exports.insertSession = async function(sess){
-    
-    var query = `INSERT INTO sessions VALUES(?,?,?,?,?,1)`;
-    await pool.query(query,
-        [sess.username,sess.sessionID,sess.createTime,sess.lastUsedTime,sess.expiryTime,sess.isActive]
-    );
-    return;
-    
-}
-exports.retrieveActiveSession = async function(username){
-    var query = `SELECT * FROM sessions WHERE username = ? AND is_active = 1`;
-    try{
-        const rows = await pool.query(query,[username]);
-        if(rows.length > 0){
-            return rows[0];
-        } else {
-            return null;
-        }
-    } catch(err){
-        throw err;
+exports.insertSession = async function(sess) {
+  var query = `INSERT INTO sessions VALUES(?,?,?,?,?,1)`;
+  await pool.query(query, [
+    sess.username,
+    sess.sessionID,
+    sess.createTime,
+    sess.lastUsedTime,
+    sess.expiryTime,
+    sess.isActive
+  ]);
+  return;
+};
+exports.retrieveActiveSession = async function(username) {
+  var query = `SELECT * FROM sessions WHERE username = ? AND is_active = 1`;
+  try {
+    const rows = await pool.query(query, [username]);
+    if (rows.length > 0) {
+      logger.log(rows[0]);
+      return {
+        username: rows[0].username,
+        sessionID: rows[0].session_id,
+        createTime: rows[0].create_time,
+        lastUsedTime: rows[0].last_used_time,
+        expiryTime: rows[0].expiry_time,
+        isActive: rows[0].is_active
+      };
+    } else {
+      return null;
     }
-}
-exports.revokeSession = function(username, sessionID){
-    var query = `UPDATE sessions SET is_active = 0 WHERE username = ? AND session_id = ?`;
-    pool.query(query,[username,sessionID]);
-    return;
-}
+  } catch (err) {
+    throw err;
+  }
+};
+exports.revokeSession = function(username, sessionID) {
+  var query = `UPDATE sessions SET is_active = 0 WHERE username = ? AND session_id = ?`;
+  try {
+    pool.query(query, [username, sessionID]);
+  } catch (e) {}
+  return;
+};
+exports.logout = async function(resBuilder, username) {
+  const sess = await exports.retrieveActiveSession(username);
+
+  //session/user not found, but whatever
+  if (sess == null) return resBuilder.success().end();
+
+  exports.revokeSession(username, sess.sessionID);
+  resBuilder.success().end();
+};
